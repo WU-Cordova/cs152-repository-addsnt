abfff402998f0b68f69e809521da536ab14e7736
addsnt <amthompson@willamette.edu>
2025-04-21 23:28:55 -0700

    fix?

--- datastructures/liststack.py
+++ datastructures/liststack.py
@@ -4,7 +4,9 @@ from typing import Generic
 
 from datastructures.linkedlist import LinkedList
 
-class ListStack[T](Generic[T], IStack[T]):
+T = TypeVar('T')
+
+class ListStack(Generic[T], IStack[T]):
     """
     ListStack (LinkedList-based Stack)
 
